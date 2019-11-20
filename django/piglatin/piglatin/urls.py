62bab9ae0980c10428a3e358dfce66e38d935fd1
dianajyoo <diana.yooj@gmail.com>
2019-11-20 14:22:36 -0500

    add views and templates

--- django/piglatin/piglatin/urls.py
+++ django/piglatin/piglatin/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^translate', views.translate, name='translate')
 ]
---
+++ django/piglatin/piglatin/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render # to render html
+
+def home(request):
+  return render(request, 'home.html')
+
+def translate(request):
+  original_text = request.GET['original_text'].lower()
+
+  translation = ''
+  for word in original_text.split():
+    if word[0] in ['a', 'e', 'i', 'o', 'u']:
+      # vowel
+      translation += word 
+      translation += 'way '
+    else:
+      # consonant
+      suffix = word[0] + 'ay '
+      translation += word[1:]
+      translation += suffix
+
+
+  return render(request, 'translate.html', {
+    'original': original_text,
+    'translation': translation
+  })
